a90abb825fb570feb8c69a23817446b399bac7c4
Dheeraj Kumar Rao <dheerajr@exzeo.com>
2019-03-08 11:26:04 +0530

    changes org name and build

--- packages/button/README.md
+++ packages/button/README.md
@@ -1,4 +1,4 @@
-# `@infinity/button`
+# `@dkrao/button`
 
 > TODO: description
 
--- packages/button/lib/button.js
+++ packages/button/lib/button.js
@@ -8,7 +8,7 @@
 
 
 import * as React from "react";
-import './styles.scss';
+
 
 
 /*
@@ -27,56 +27,9 @@ import './styles.scss';
 */
 
 export default class button extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    renderIcon = (inverse) => {
-        const {icon, title} = this.props;
-        if(inverse){
-            return(
-                <>
-                    {icon}
-                    {title}
-                </>
-            );
-        }
-
-        return(
-            <>
-                {title}
-                {icon}
-            </>
-        )
-    };
-
     render(){
-        const {
-            hrefLink,
-            type,
-            styleClass,
-            onButtonClick,
-            onButtonOver,
-            title,
-            isDisabled,
-            icon,
-            target,
-            iconSide
-           } = this.props;
-
-        const redirectLink = (hrefLink === "" || hrefLink == null || hrefLink == undefined) ? "javascript:void(0)" : hrefLink
         return(
-            <this.props.type
-                href={redirectLink}
-                className={`infinity-button ${styleClass} ${isDisabled ? 'ButtonDisabled':null}`}
-                onClick={onButtonClick}
-                onmouseover={onButtonOver}
-                disabled={isDisabled}
-                target ={target}
-            >
-            {
-                (iconSide && iconSide.toLowerCase() == 'left') ? this.renderIcon(true):this.renderIcon()
-            }
-            </this.props.type>
+           <button>{'hello button NPM'}</button>
         )
     }
 }
