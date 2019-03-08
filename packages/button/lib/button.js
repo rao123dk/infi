6e70a4bd05d216767bb59a9aad1d46e6069baa01
Dheeraj Kumar Rao <dheerajr@exzeo.com>
2019-03-08 11:53:13 +0530

    changes buld path 2

--- packages/button/lib/button.js
+++ packages/button/lib/button.js
@@ -1,36 +1,11 @@
-'use strict';
-
-// module.exports = button;
-
-// function button() {
-//     // TODO
-// }
 
 
-import * as React from "react";
-
-
-
-/*
-@title - Button label
-@type - its tag type(a,button)
-@styleClass - proved your own class name
-@href - for redirect you can pass your link
-@icon - its component to insert in buuton label
-@onButtonClick - do some action on button click
-@onmouseover - do some action on button hover
-@isDisabled - disable button on the basisof props
-
-@target - if type is a tag
-
-@iconSide - left or right
-*/
-
-export default class button extends React.Component {
-    render(){
-        return(
-           <button>{'hello button NPM'}</button>
-        )
-    }
+import React, { Component } from 'react';
+class Button extends React.Component {
+  render() {
+    return (
+      <button>{"Hello Button"}</button>
+    );
+  }
 }
-
+export default Button;
